84a2a3b5d6302b88a2490e9160ef21f16bef4d88
MercyLM <MercyLM@gmail.com>
2020-04-15 14:48:53 +0800

    Vue-cli 專案建立

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="這是 Vue.js App 您好"/>
   </div>
 </template>
 
