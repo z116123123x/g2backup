908bc75422ebcfe13366dd38c0f15b88ea4edfa5
MercyLM - Home <kevin5151112@gmail.com>
2020-04-18 20:54:33 +0800

    add footer

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -2,14 +2,18 @@
   <div class="components">
     <p>/components/btn-more.vue</p>
     <ButtonMore />
+    <p>/layout/footer.vue</p>
+    <Footer />
   </div>
 </template>
 <script>
 import ButtonMore from "@/components/btn-more.vue";
+import Footer from "@/layout/footer.vue";
 
 export default {
   components: {
-    ButtonMore
+    ButtonMore,
+    Footer
   }
 };
 </script>
