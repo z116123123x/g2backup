bd63a1c7de9a498e42251f8f8c9b42781a09bd32
SiaoXiao <kelu0427@gmail.com>
2020-04-23 23:44:05 +0800

    shop_update_scss

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,17 +1,27 @@
 <template>
   <main class="wrapper">
     <section class="show_item">
-      <img src="../assets/search_logo_img.svg" width="170px" height="100px" class="search_logo" />
+      <img
+        src="../assets/search_logo_img.svg"
+        width="170px"
+        height="100px"
+        class="search_logo"
+      />
       <div></div>
-      <img src="../assets/market_Img.svg" width="171px" height="153px" class="market" />
+      <img
+        src="../assets/market_Img.svg"
+        width="171px"
+        height="153px"
+        class="market"
+      />
     </section>
 
     <section class="shop_main">
-      <div class="container-wrapper">
+      <div class="search-wrapper">
         <!-- <img src="../assets/search_logo_img.svg" width="170px" height="100px" class="search_logo"> -->
         <div class="search-section">
           <div class="searchbar">
-            <div class="searchbar__main">
+            <div class="searchbar_main">
               <form role="search" class="searchbar-input" autocomplete="off">
                 <input
                   class="searchbar-input_input"
@@ -42,15 +52,23 @@
         </div>
         <!-- <img src="../assets/market_Img.svg" width="171px" height="153px" class="market"> -->
       </div>
+
       <div class="blog_line_box">
         <img src="../assets/blog_line.svg" class="blog_line" width="90%" />
       </div>
 
       <div class="shop_container">
         <aside class="filter-panel">
-          <img src="../assets/market_choose_right_mountain_only.svg" class="mountain" />
+          <img
+            src="../assets/market_choose_right_mountain_only.svg"
+            class="mountain"
+          />
           <div class="filter-status">
-            <img src="../assets/icon/filter.svg" width="20px" style="margin-right: 10px;" />
+            <img
+              src="../assets/icon/filter.svg"
+              width="20px"
+              style="margin-right: 10px;"
+            />
             <div class="filter-status_text">條件篩選</div>
           </div>
           <form class="filter-group">
@@ -58,25 +76,25 @@
             <div class="folding-items">
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="24hour" />
-                    <span class="checkbox__label">24小時到府</span>
+                    <span class="checkbox_label">24小時到府</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="home" />
-                    <span class="checkbox__label">宅配到府</span>
+                    <span class="checkbox_label">宅配到府</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="store" />
-                    <span class="checkbox__label">超商取貨</span>
+                    <span class="checkbox_label">超商取貨</span>
                   </label>
                 </div>
               </div>
@@ -87,57 +105,57 @@
             <div class="folding-items">
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="gift_box" />
-                    <span class="checkbox__label">禮盒</span>
+                    <span class="checkbox_label">禮盒</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="promotion" />
-                    <span class="checkbox__label">促銷商品</span>
+                    <span class="checkbox_label">促銷商品</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="coming_soon" />
-                    <span class="checkbox__label">即將放送</span>
+                    <span class="checkbox_label">即將放送</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="machining" />
-                    <span class="checkbox__label">加工產品</span>
+                    <span class="checkbox_label">加工產品</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="dried_fruit" />
-                    <span class="checkbox__label">果乾</span>
+                    <span class="checkbox_label">果乾</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="fresh_fruit" />
-                    <span class="checkbox__label">新鮮水果</span>
+                    <span class="checkbox_label">新鮮水果</span>
                   </label>
                 </div>
               </div>
               <div class="checkbox-filter">
                 <div class="checkbox">
-                  <label class="checkbox__control">
+                  <label class="checkbox_control">
                     <input type="checkbox" value="organic_fruit" />
-                    <span class="checkbox__label">有機水果</span>
+                    <span class="checkbox_label">有機水果</span>
                   </label>
                 </div>
               </div>
@@ -146,9 +164,16 @@
         </aside>
 
         <aside class="hot_commodity">
-          <img src="../assets/market_choose_right_mountain_only.svg" class="mountain" />
+          <img
+            src="../assets/market_choose_right_mountain_only.svg"
+            class="mountain"
+          />
           <div class="hot_commodity_filter-status">
-            <img src="../assets/icon/podium.svg" width="30px" style="margin-right: 10px;" />
+            <img
+              src="../assets/icon/podium.svg"
+              width="30px"
+              style="margin-right: 10px;"
+            />
             <div class="hot_commoditystatus_text">明星商品</div>
           </div>
           <div>
@@ -165,7 +190,11 @@
         <div class="search-item-result_items">
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -174,7 +203,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -190,7 +225,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -199,7 +238,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -215,7 +260,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -224,7 +273,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -240,7 +295,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -249,7 +308,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -265,7 +330,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -274,7 +343,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -290,7 +365,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -299,7 +378,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -315,7 +400,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -324,7 +413,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -340,7 +435,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -349,7 +448,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -365,7 +470,11 @@
           </div>
           <div class="commodity">
             <div class="card_img_box">
-              <img src="../assets/ia_300000017.jpg" width="270px" height="100%" />
+              <img
+                src="../assets/ia_300000017.jpg"
+                width="270px"
+                height="100%"
+              />
             </div>
 
             <div class="card_content">
@@ -374,7 +483,13 @@
               </div>
 
               <div class="card_tag">
-                <img src="../assets/icon/tag.svg" alt width="16px" height="16px" class="tag_icon" />
+                <img
+                  src="../assets/icon/tag.svg"
+                  alt
+                  width="16px"
+                  height="16px"
+                  class="tag_icon"
+                />
                 <span class="card_tag_text">24hr宅配到府</span>
               </div>
 
@@ -395,7 +510,9 @@
     <nav aria-label="Page navigation example">
       <ul class="pagination justify-content-center">
         <li class="page-item disabled">
-          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true"
+            >Previous</a
+          >
         </li>
         <li class="page-item">
           <a class="page-link" href="#">1</a>
