a86f809b5198eb0e4916f2370053e3c772fe4641
MercyLM - Home <kevin5151112@gmail.com>
2020-04-25 14:44:57 +0800

    update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,52 +12,51 @@ const routes = [{
 {
   path: '/book',
   name: 'Book',
-  component: () => { import("@/views/Book.vue"); },
+  component: () => import("@/views/Book.vue"),
 },
 {
   path: '/blog-landing',
   name: 'Blog-landing',
-  component: () => { import("@/views/Blog-landing.vue"); },
+  component: () => import("@/views/Blog-landing.vue"),
 },
 {
   path: '/blog-post',
   name: 'Blog-post',
-  component: () => { import("@/views/Blog-post.vue"); },
+  component: () => import("@/views/Blog-post.vue"),
 },
 {
-
   path: '/shop',
   name: 'Shop',
-  component: () => { import("@/views/Shop.vue"); },
+  component: () => import("@/views/Shop.vue"),
 },
 {
   path: '/member',
   name: 'Member',
-  component: () => { import("@/views/Member.vue"); },
+  component: () => import("@/views/Member.vue"),
   children: [{
     path: "track",
-    component: () => { import("@/views/track.vue"); },
+    component: () => import("@/views/track.vue"),
   },
   {
     // children 指的是 member router（路由）內的"子頁"，例如網址只要符合 /member/information 就會嵌入 Member.vue 樣板及 Information.vue 元件
     path: "information",
-    component: () => { import("@/views/Information.vue"); },
+    component: () => import("@/views/Information.vue"),
   },
   {
     path: "order",
-    component: () => { import("@/views/Order.vue"); },
+    component: () => import("@/views/Order.vue"),
   }
   ]
 },
 {
   path: '/adminLogin',
   name: 'AdminLogin',
-  component: () => { import("@/views/AdminLogin.vue"); },
+  component: () => import("@/views/AdminLogin.vue"),
 },
 {
   path: '/adminManage',
   name: 'AdminManage',
-  component: () => { import("@/views/AdminManage.vue"); },
+  component: () => import("@/views/AdminManage.vue"),
 },
 ];
 
