e6f6a65718ef02b6bc2ce31d19ff84ca1b4694bf
MercyLM <MercyLM@gmail.com>
2020-04-28 20:48:18 +0800

    update shopping

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -1,37 +1,50 @@
 <template>
-    <div class="contain">
-        <aside class="left">
-            <div class="head_portrait">
-                <div class="head_img_box">
-                    <img src="../assets/navbar_account_img.svg" alt="">
-                </div>
-                <ul>
-                    <li>設定頭像</li>
-                    <li class="nick">暱稱</li>
-                    <li class="fans">身分別:果粉</li>
-                </ul>
-            </div>
+  <div class="contain">
+    <aside class="left">
+      <div class="head_portrait">
+        <div class="head_img_box">
+          <img src="../assets/navbar_account_img.svg" alt />
+        </div>
+        <ul>
+          <li>設定頭像</li>
+          <li class="nick">暱稱</li>
+          <li class="fans">身分別:果粉</li>
+        </ul>
+      </div>
 
-            <div class="info">
-                <ul>
-                    <li><router-link to="/main/member/update">個人資料修改</router-link></li>
-                    <li><router-link to="/main/member/order">訂單管理</router-link></li>
-                    <li><router-link to="/main/member/track">追蹤名單管理</router-link></li>
-                    <li><router-link to="/main/member/shopping">購物籃</router-link></li>
-                </ul>
-            </div>
-            <div class="farm_button">
-                <div class="farm">
-                    <router-link to="/main/member/farmRegistered"><p>成為果農</p></router-link>
-                </div>
-            </div>
-            <div class="farm_button">
-                <div class="farm">
-                    <router-link to="/farm"><p>切換果農</p></router-link>
-                </div>
-            </div>
-        </aside>
-        <router-view />
-        
-    </div>
+      <div class="info">
+        <ul>
+          <li>
+            <router-link to="/main/member/update">個人資料修改</router-link>
+          </li>
+          <li>
+            <router-link to="/main/member/order">訂單管理</router-link>
+          </li>
+          <li>
+            <router-link to="/main/member/track">追蹤名單管理</router-link>
+          </li>
+          <li>
+            <router-link to="/main/member/shopping">購物籃</router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="farm_button">
+        <div class="farm">
+          <router-link to="/main/member/farmRegistered">
+            <p>成為果農</p>
+          </router-link>
+        </div>
+      </div>
+      <div class="farm_button">
+        <div class="farm">
+          <router-link to="/farm">
+            <p>切換果農</p>
+          </router-link>
+        </div>
+      </div>
+    </aside>
+    <keep-alive>
+      <router-view />
+    </keep-alive>
+  </div>
 </template>
