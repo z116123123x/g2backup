3e49c440eacadb12ced21619819a2e058f2bf225
MercyLM - Home <kevin5151112@gmail.com>
2020-05-01 23:47:45 +0800

    update shopping

--- src/views/MemberCheckInfo.vue
+++ src/views/MemberCheckInfo.vue
@@ -5,6 +5,17 @@
       <div class="form">
         <div class="form-content">
           <p class="title">訂購人資訊：</p>
+          <div class="info-box">
+            <p class="required">姓名：<input type="text"></p>
+            <p class="required">手機：<input type="text"></p>
+            <p>電話：<input type="text"></p>
+            <p class="required">信箱：<input type="text"></p>
+            <p class="required">收件地址：</p>
+            <p class="required">收件時間：</p>
+          </div>
+          <p class="title">選擇付款方式：</p>
+          <div class="pay-box"></div>
+          <div class="btn-box"></div>
         </div>
       </div>
     </div>
