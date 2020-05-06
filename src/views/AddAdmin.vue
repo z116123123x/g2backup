b9bfc4ad3e186b831d73d5db0beb12f8b090d793
lesliezsy <56362727+lesliezsy@users.noreply.github.com>
2020-05-06 16:10:47 +0800

    0506 樣式微調 補回上一頁

--- src/views/AddAdmin.vue
+++ src/views/AddAdmin.vue
@@ -11,7 +11,6 @@
           <th>編號</th>
           <td>xxxxxx</td>
         </tr>
-
         <tr>
           <th>
             <label for="addAdminName">姓名</label>
@@ -40,7 +39,7 @@
           <th></th>
           <td>
           <div class="adAdminBtnSec">
-            <input id="addAdminCancel" type="submit" value="取消" />
+            <input id="addAdminCancel" type="button" onClick="javascript:history.back(1)" value="取消" />
             <input id="addAdminSubmit" type="submit" value="送出" />
           </div>
           </td>
