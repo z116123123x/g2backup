fc9cb3f26b68e6a2cfeff82ebf840ad8ba063c3f
MercyLM <MercyLM@gmail.com>
2020-05-07 19:35:47 +0800

    add MemberLogin php

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
         disableHostCheck: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:8888',
+                target: 'http://localhost',
                 ws: true,
                 changeOrigin: true,
                 autoRewrite: true
