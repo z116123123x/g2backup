9111fd13be125f65992c15a280c101a2a36e4ff2
MercyLM <MercyLM@gmail.com>
2020-05-07 11:54:45 +0800

    update book

--- src/views/BookSpring.vue
+++ src/views/BookSpring.vue
@@ -1,45 +1,95 @@
 <template>
   <div class="bk_Spring">
-    <component :is="currentTab"></component>
+    <component :page="currentTab.page" :is="currentTab.index"></component>
     <div class="change_bookpage">
-      <div v-for="tab in tabs" :key="tab.id" :class="'changebutton' + tab.class">
-        <button @click="toggleTab(tab)"></button>
+      <div v-for="tab in tabs" :key="tab.name" :class="'changebutton' + tab.class">
+        <button v-if="tab.status == true" @click="toggleTab(tab)"></button>
       </div>
     </div>
   </div>
 </template>
 <script>
-import before from "@/views/BookSeasonPage1";
-import after from "@/views/BookSeasonPage2";
+import index from "@/views/BookSeasonIndex";
+import page1 from "@/views/BookSeasonPage1";
+import page2 from "@/views/BookSeasonPage2";
 export default {
   data() {
     return {
-        
-      currentTab: "before",
+      currentTab: [],
       tabs: [
         {
           name: "上一頁",
-          index: "before",
           class: "left",
-          id: 1
+          status: false
         },
         {
           name: "下一頁",
-          index: "after",
           class: "right",
-          id: 2
+          status: true
         }
       ]
     };
   },
+  created() {
+    this.currentTab = {
+      index: "index",
+      page: 0
+    };
+  },
   methods: {
     toggleTab: function(tab) {
-      this.currentTab = tab.index;
+      // 頁面邏輯判斷
+      if (this.currentTab.page == 0) {
+        this.currentTab.index = "page1";
+        this.currentTab.page = 1;
+      } else if (
+        this.currentTab.page > 0 &&
+        this.currentTab.index == "page1" &&
+        tab.name == "下一頁"
+      ) {
+        this.currentTab.index = "page2";
+        this.currentTab.page++;
+      } else if (
+        this.currentTab.page > 0 &&
+        this.currentTab.index == "page2" &&
+        tab.name == "下一頁"
+      ) {
+        this.currentTab.index = "page1";
+        this.currentTab.page++;
+      } else if (
+        this.currentTab.page == 1 &&
+        this.currentTab.index == "page1" &&
+        tab.name == "上一頁"
+      ) {
+        this.currentTab.index = "index";
+        this.currentTab.page--;
+      } else if (
+        this.currentTab.page > 0 &&
+        this.currentTab.index == "page1" &&
+        tab.name == "上一頁"
+      ) {
+        this.currentTab.index = "page2";
+        this.currentTab.page--;
+      } else if (
+        this.currentTab.page > 0 &&
+        this.currentTab.index == "page2" &&
+        tab.name == "上一頁"
+      ) {
+        this.currentTab.index = "page1";
+        this.currentTab.page--;
+      }
+      // 按鈕顯示切換
+      if (this.currentTab.page == 1) {
+        this.tabs[0].status = true;
+      } else if (this.currentTab.page == 0) {
+        this.tabs[0].status = false;
+      }
     }
   },
   components: {
-    before,
-    after
+    index,
+    page1,
+    page2
   }
 };
 </script>
