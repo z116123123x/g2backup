1c1f07fedc505b383538bd1dc781ef80d54f8dd0
MercyLM <MercyLM@gmail.com>
2020-05-13 16:48:56 +0800

    update book

---
+++ src/components/bookSpring.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="fruit_start">
+    <h1>梅子</h1>
+    <div class="fruit_start_img">
+      <div class="img_content">
+        <img src="@/assets/book_img/spring/plum/plum04.jpg" alt />
+      </div>
+    </div>
+  </div>
+</template>
