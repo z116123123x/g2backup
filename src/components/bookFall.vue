2680179e213a38bbe4f803ffeba90328fd38b9e4
abrillin <sarah860313@gmail.com>
2020-05-14 15:57:57 +0800

    xxxxxx

--- src/components/bookFall.vue
+++ src/components/bookFall.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fruit_start">
-    <h1 style="color:#e19b6b;" class="fall_fruit_title">柿子</h1>
+    <h1>柿子</h1>
     <div class="fruit_start_img fruit_start_img_fall">
       <div class="img_content">
         <img src="@/assets/book_img/spring/plum/plum04.jpg" alt />
