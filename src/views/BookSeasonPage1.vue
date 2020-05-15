33273d7077ea22b2d5f87ce0075a97fa0345959b
MercyLM <MercyLM@gmail.com>
2020-05-15 10:48:27 +0800

    補充說明、修改首頁的連結路徑

--- src/views/BookSeasonPage1.vue
+++ src/views/BookSeasonPage1.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="book_layout">
     <div class="bk_ct bk_ct_left">
+      <!-- 9. 接收到 contentType、page 的值，傳值到元件的自定義變數 type、page -->
       <component :is="'PageLeft'+this.left" :type="contentType" :page="page"></component>
       <div class="fruit_intro">
         <p>梅子是亞洲特有的果樹，除了灣、日本和韓國之外，其他地區很少栽種。梅子的風味以清酸稱絕，滿口生津而別有風味，和一般水果講求香、甜有所不同。梅子所含的果酸較多，如果生食未成熟的青梅會造成腸胃不適，經過醃漬、曝晒加工之後更適合食用。梅子的加工品項多樣，除梅乾、蜜餞、脆梅、梅醬等，也能用來釀酒。梅子內有豐富檸檬酸、蘋果酸等有機酸，蛋白質的含量更是草莓、柑桔的兩倍以上。台灣的梅子主要栽種在中央山脈兩側，以南投信義鄉、水里鄉的產量最多，信義鄉更有「梅子鄉」的雅稱。</p>
